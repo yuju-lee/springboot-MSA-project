c4904d04c3ad7150044cccc588c9319f37940fe4
yuju-lee <yuju-lee@github.com>
2024-06-21 01:56:52 +0900

    update welcome message and refine error message

--- src/main/java/com/sparta/springproject/controller/MemberController.java
+++ src/main/java/com/sparta/springproject/controller/MemberController.java
@@ -5,8 +5,10 @@ import com.sparta.springproject.dto.MemberDTO;
 import com.sparta.springproject.model.Member;
 import com.sparta.springproject.service.MemberService;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,14 +28,24 @@ public class MemberController {
     public ResponseEntity<?> registerUser(@RequestBody MemberDTO memberDTO) {
         try {
             Member member = memberService.registerUser(memberDTO);
-            return ResponseEntity.ok(member);
+            String welcomeMessage = "Welcome, " + memberDTO.getUserName() + "!";
+            return ResponseEntity.ok(welcomeMessage);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequestDTO requestDto, HttpServletResponse res) {
-        return memberService.login(requestDto, res);
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequestDTO loginRequestDTO, HttpServletResponse response, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(bindingResult.getAllErrors());
+        }
+
+        try {
+            memberService.login(loginRequestDTO, response);
+            return ResponseEntity.ok("Login successful!");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
--- src/main/java/com/sparta/springproject/dto/MemberDTO.java
+++ src/main/java/com/sparta/springproject/dto/MemberDTO.java
@@ -2,13 +2,23 @@ package com.sparta.springproject.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 @Getter
 @Setter
 public class MemberDTO {
+    @NotNull(message = "Email cannot be null")
+    @NotEmpty(message = "Email cannot be empty")
     private String email;
+
+    @NotNull(message = "Username cannot be null")
+    @NotEmpty(message = "Username cannot be empty")
     private String userName;
+
+    @NotNull(message = "Password cannot be null")
+    @NotEmpty(message = "Password cannot be empty")
     private String password;
-    private String phone;
-    private String address;
+
+    // Getters and Setters
 }
