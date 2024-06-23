c6b83d3cb5962514eefc04d7f01048a7ab84791b
yuju-lee <periq23@gmail.com>
2024-06-23 13:45:47 +0900

    feature: login with access token / refresh token

--- src/main/java/com/sparta/springproject/controller/AuthController.java
+++ src/main/java/com/sparta/springproject/controller/AuthController.java
@@ -34,20 +34,24 @@ public class AuthController {
     private final JwtUtil jwtUtil;
     private final TokenService tokenService;
 
+//    @PostMapping("/login")
+//    public ResponseEntity<?> login(@RequestBody LoginRequestDTO request, HttpServletResponse response) {
+////        Authentication authentication = authenticationManager.authenticate(
+////                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+////        );
+////
+////        SecurityContextHolder.getContext().setAuthentication(authentication);
+////        MemberEntity member = (MemberEntity) authentication.getPrincipal();
+////        String accessToken = jwtTokenProvider.createAccessToken(member.getEmail());
+////        String refreshToken = jwtTokenProvider.createRefreshToken(member.getEmail());
+//
+////
+////        return ResponseEntity.ok(new AuthResponse(accessToken, refreshToken));
+//    }
+
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequestDTO request, HttpServletResponse response) {
-//        Authentication authentication = authenticationManager.authenticate(
-//                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
-//        );
-//
-//        SecurityContextHolder.getContext().setAuthentication(authentication);
-//        MemberEntity member = (MemberEntity) authentication.getPrincipal();
-//        String accessToken = jwtTokenProvider.createAccessToken(member.getEmail());
-//        String refreshToken = jwtTokenProvider.createRefreshToken(member.getEmail());
-        authService.login(request, response);
-        return ResponseEntity.ok("Login successful");
-//
-//        return ResponseEntity.ok(new AuthResponse(accessToken, refreshToken));
+        return authService.login(request, response);
     }
 
     @PostMapping("/logout")
