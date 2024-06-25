a0c2398c3158364426be21daa6f347517e6306d9
yuju-lee <periq23@gmail.com>
2024-06-25 12:03:07 +0900

    Feature: implement wishlist

--- src/main/java/com/sparta/springproject/repository/WishListRepository.java
+++ src/main/java/com/sparta/springproject/repository/WishListRepository.java
@@ -4,8 +4,11 @@ import com.sparta.springproject.model.WishEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface WishListRepository extends JpaRepository<WishEntity, Long> {
     List<WishEntity> findByEmail(String email);
+
+    Optional<WishEntity> findByProductIdAndEmail(Integer productId, String email);
 }
